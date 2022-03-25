30585ed067b15075fed093cd36d82094228d320a
Alexandreferreiracampos <50953839+Alexandreferreiracampos@users.noreply.github.com>
2022-03-25 14:10:16 -0300

    crypt password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,12 +4,19 @@ const User = require('../model/Usuarios');
 const router = express.Router();
 
 router.post('/register', async (req, res)=>{
+    const {email} = req.body;
     try{
+
+        if(await User.findOne({ email }))
+            return res.status(400).send({Erro: "Usuário já existe."});
+        
         const user = await User.create(req.body);
-        return res.send({user});
+        user.password = undefined;
+        
+        return res.status(200).send({user});
 
     }catch(err){
-        return res.status(400).send({error:'Registration failed'});
+        return res.status(400).send({error:'Falha ao registrar nova Usuario.'});
 
     }
 })
--- src/model/Usuarios.js
+++ src/model/Usuarios.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,14 @@ const UserSchema = new mongoose.Schema({
     }
 
 });
+
+//crypt password
+UserSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+})
 
 module.exports = mongoose.model("User", UserSchema);
 
